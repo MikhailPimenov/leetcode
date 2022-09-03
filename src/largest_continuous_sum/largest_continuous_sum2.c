3ec212c9c1835292580ed6142367dc81c881ea5a
Elvia Attack <elviaatt@mi-p3.msk.21-school.ru>
2022-09-03 20:20:07 +0300

    develop/largest_continuous_sum2 fixed

--- src/largest_continuous_sum/largest_continuous_sum2.c
+++ src/largest_continuous_sum/largest_continuous_sum2.c
@@ -103,7 +103,8 @@ int largest_continuous_sum3(const int* array, int length) {
     int left = 0; 
     int right = 0;
     int sum = array[left];
-    int best = 0;
+    int best = sum;
+ 
 
     while (_go_on(left, right, length)) {
         if (_should_expand(sum, right, length)) 
@@ -278,6 +279,126 @@ void test_largest_continuous_sum(int (*algorithm)(const int*, int)) {
     
 }
 
+
+
+void print_input(const int* input, int length) {
+    for (int index = 0; index < length; ++index) 
+        printf("%d ", input[index]);
+    printf("\n");
+}
+
+void test_case(
+    int test_number,
+    const int* input,
+    int length,
+    int expected,
+    int actual
+) {
+    printf("Test  #%d:\t", test_number);
+    if (actual == expected) {
+        printf("ok\n");
+    } else {
+        printf("FAILED\n");
+        printf("input:\t\t");
+        print_input(input, length);
+        printf("expected:\t%d\n", expected);
+        printf("actual:\t\t%d\n", actual);
+    }
+}
+
+void largest_continuous_sum_test(int (*algorithm)(const int*, int)) {
+    printf("largest_continuous_sum\n");
+
+    {
+        const int input[] = {5,5,5};
+        const int length = sizeof(input)/sizeof(input[0]);
+        const int expected = 15;
+        const int actual = algorithm(input, length);
+        test_case(1, input, length, expected, actual);
+    }
+
+    {
+        const int input[] = {-101, 5, 5, -102};
+        const int length = sizeof(input)/sizeof(input[0]);
+        const int expected = 10;
+        const int actual = algorithm(input, length);
+        test_case(2, input, length, expected, actual);
+    }
+
+    {
+        const int input[] = {-101, 5, 5, -5, 5, 5, 5};
+        const int length = sizeof(input)/sizeof(input[0]);
+        const int expected = 20;
+        const int actual = algorithm(input, length);
+        test_case(3, input, length, expected, actual);
+    }
+
+    {
+        const int input[] = {5, 5, 5, -5, 5, 5, 5, -101, -102, 5, 5};
+        const int length = sizeof(input)/sizeof(input[0]);
+        const int expected = 25;
+        const int actual = algorithm(input, length);
+        test_case(4, input, length, expected, actual);
+    }
+
+    {
+        const int input[] = {5, 5, -1001, 102, -1003};
+        const int length = sizeof(input)/sizeof(input[0]);
+        const int expected = 102;
+        const int actual = algorithm(input, length);
+        test_case(5, input, length, expected, actual);
+    }
+
+    {
+        const int input[] = {5, 5, -1000, 100, -10000, 200};
+        const int length = sizeof(input)/sizeof(input[0]);
+        const int expected = 200;
+        const int actual = algorithm(input, length);
+        test_case(6, input, length, expected, actual);
+    }
+    
+    {
+        const int input[] = {5, 5, -1000, 100, -10000, 200, -5, -5, 200, -1000};
+        const int length = sizeof(input)/sizeof(input[0]);
+        const int expected = 390;
+        const int actual = algorithm(input, length);
+        test_case(7, input, length, expected, actual);
+    }
+    
+    {
+        const int input[] = {100};
+        const int length = sizeof(input)/sizeof(input[0]);
+        const int expected = 100;
+        const int actual = algorithm(input, length);
+        test_case(8, input, length, expected, actual);
+    }
+    
+    {
+        const int input[] = {-1000, 10};
+        const int length = sizeof(input)/sizeof(input[0]);
+        const int expected = 10;
+        const int actual = algorithm(input, length);
+        test_case(9, input, length, expected, actual);
+    }
+    
+    {
+        const int input[] = {-100, 10, -10000};
+        const int length = sizeof(input)/sizeof(input[0]);
+        const int expected = 10;
+        const int actual = algorithm(input, length);
+        test_case(10, input, length, expected, actual);
+    }
+    
+    
+    {
+        const int input[] = {-100, -10, -10000};
+        const int length = sizeof(input)/sizeof(input[0]);
+        const int expected = 0;
+        const int actual = algorithm(input, length);
+        test_case(11, input, length, expected, actual);
+    }
+}
 int main() {
     test_largest_continuous_sum(largest_continuous_sum3);
+    largest_continuous_sum_test(largest_continuous_sum3);
 }
