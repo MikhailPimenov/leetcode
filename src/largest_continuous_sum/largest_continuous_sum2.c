f4ff623879ebfebcbd7811db3b6a9aff715eb40d
Elvia Attack <elviaatt@mi-p3.msk.21-school.ru>
2022-09-03 20:15:34 +0300

    develop/largest_continuous_sum2.c works

--- src/largest_continuous_sum/largest_continuous_sum2.c
+++ src/largest_continuous_sum/largest_continuous_sum2.c
@@ -1,34 +1,122 @@
 #include "stdio.h"
-//  -10 15 -5 -5 -5 5 5
+//  -10 20 -5 -5 -5 10 10 -5
+//  -10 200 -50 -50 -50 10 10 -5
 int largest_continuous_sum2(const int* array, int length) {
-
     int left = 0; 
     int right = 0;
-
     int sum = array[left];
     int best = 0;
-    while (right < length && left < length) {
-        if (sum + array[right] > 0) {
-            sum += array[right];
-            ++right;
-        } else {
-            sum -= array[left];
-            ++left;
-        }
+    while (1) {
+        if (sum < 0) {  //  shrinking
 
-        if (sum > best)
-            best = sum;
+            if (left < right) {  //  can shrink
+                
+                ++left;
+                sum -= array[left];
 
-        if (right == left) {
-            ++right;
-            ++left;
-            sum = array[left];
+                if (sum > best)
+                    best = sum;
 
-        }
+            } else if (right + 1 < length) {  // cannot shrink => stepping over 
+
+                ++left;
+                ++right;
+                sum = array[left];
+
+                if (sum > best)
+                    best = sum;
+
+            } else {
+                break;
+            }
+        
+        } else {  //  expanding
+            if (right + 1 < length) {
+                ++right;
+                sum += array[right];
+
+                if (sum > best)
+                    best = sum;
+            } else {
+                ++left;
+                sum -= array[left];
+
+                if (sum > best)
+                    best = sum;
+            }
+        }       
+        
+        if (left == length - 1 && right == length - 1)
+            break;
     }
 
     return best > 0 ? best : 0;
-    // return -1 + 0 * length + 0 * *array;
+}
+
+
+int _go_on(int left, int right, int length) {
+    return left + 1 < length && right + 1 < length;
+}
+
+int _should_expand(int sum, int right, int length) {
+    if (sum < 0) 
+        return 0;
+
+    if (right + 1 >= length)
+        return 0;
+
+    return 1;
+}
+
+int _should_shrink(int sum, int left, int right, int length) {
+    if (right + 1 >= length && left + 1 < length)
+        return 1;
+
+    if (sum < 0 && left < right)
+        return 1;
+
+    return 0; 
+}
+
+void _expand(const int* array, int* sum, int* right) {
+    ++(*right);
+    (*sum) += array[*right];
+}
+
+void _shrink(const int* array, int* sum, int* left) {
+    (*sum) -= array[*left];
+    ++(*left);
+}
+
+void _step_over(const int* array, int* sum, int* left, int* right) {
+    ++(*right);
+    ++(*left);
+    (*sum) = array[*left];
+}
+
+void _update(int sum, int* best) {
+    if (sum > *best)
+        *best = sum;
+}
+
+int largest_continuous_sum3(const int* array, int length) {
+    int left = 0; 
+    int right = 0;
+    int sum = array[left];
+    int best = 0;
+
+    while (_go_on(left, right, length)) {
+        if (_should_expand(sum, right, length)) 
+            _expand(array, &sum, &right);
+        else if (_should_shrink(sum, left, right, length))
+            _shrink(array, &sum, &left);
+        else   
+            _step_over(array, &sum, &left, &right);
+
+        _update(sum, &best);  
+    }
+
+    return best > 0 ? best : 0;
 }
 
 void print_array(const int* array, int length) {
@@ -154,9 +242,42 @@ void test_largest_continuous_sum(int (*algorithm)(const int*, int)) {
         ++test_number;
     }
 
+    {
+        const int input[] = { -10, 20, -5, -5, -5, 10, 10, -5 };
+        const int length = sizeof(input) / sizeof(input[0]);
+
+        const int expected = 25;
+        const int actual = algorithm(input, length);
+
+        print_test_case(test_number, actual, expected, input, length);
+        ++test_number;
+    }
+
+    {
+        const int input[] = { -10, 200, -50, -50, -50, 10, 10, -5 };
+        const int length = sizeof(input) / sizeof(input[0]);
+
+        const int expected = 200;
+        const int actual = algorithm(input, length);
+
+        print_test_case(test_number, actual, expected, input, length);
+        ++test_number;
+    }
+
+    {
+        const int input[] = { 10, -100, 1000, -10000, 100000, -1000000 };
+        const int length = sizeof(input) / sizeof(input[0]);
+
+        const int expected = 100000;
+        const int actual = algorithm(input, length);
+
+        print_test_case(test_number, actual, expected, input, length);
+        ++test_number;
+    }
+
     
 }
 
 int main() {
-    test_largest_continuous_sum(largest_continuous_sum2);
+    test_largest_continuous_sum(largest_continuous_sum3);
 }
