9213bb8186eb5547fefa6c572cf902d217ff9533
MikhailPimenov <misha4git@yandex.ru>
2022-12-15 21:30:04 +0300

    home/product of first and second maximum - get_product_test primitive function

--- src/product_of_first_and_second_maximum/product_of_first_and_second_maximum4.c
+++ src/product_of_first_and_second_maximum/product_of_first_and_second_maximum4.c
@@ -17,10 +17,48 @@ int get_product(const int* array, int length) {
     return 0;
 }
 
+void get_product_test() {
+    {
+        const int array[] = {1,2,3,4,5,6};
+        const int length = sizeof(array) / sizeof(array[0]);
+
+        const int expected = 30;
+        const int actual = get_product(array, length);
+
+        printf("test #1:\t");
+        if (actual == expected) {
+            printf("ok\n");
+        } else {
+            printf("FAILED\n");
+            printf("input:\t\t");
+            print_array(array, length);
+            printf("expected:\t%d\n", expected);
+            printf("actual:\t\t%d\n", actual);
+        }
+    }
+
+    {
+        const int array[] = {9,2,7,4,5,6};
+        const int length = sizeof(array) / sizeof(array[0]);
+
+        const int expected = 63;
+        const int actual = get_product(array, length);
+
+        printf("test #2:\t");
+        if (actual == expected) {
+            printf("ok\n");
+        } else {
+            printf("FAILED\n");
+            printf("input:\t\t");
+            print_array(array, length);
+            printf("expected:\t%d\n", expected);
+            printf("actual:\t\t%d\n", actual);
+        }
+    }
+}
+
 int main() {
-    const int array_main[] = {1,2,3,4,5,6,7,8};
-    const int length_main = sizeof(array_main) / sizeof(array_main[0]);
-    print_array(array_main, length_main);
+    get_product_test();
 
     return 0;
 }
