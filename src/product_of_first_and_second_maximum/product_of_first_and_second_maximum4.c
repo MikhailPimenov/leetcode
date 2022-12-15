ae8055b07acf2ad3f7a1b55a18160baa5ef356f9
MikhailPimenov <misha4git@yandex.ru>
2022-12-15 21:14:35 +0300

    home/product of first and second maximum - empty get_product function

--- src/product_of_first_and_second_maximum/product_of_first_and_second_maximum4.c
+++ src/product_of_first_and_second_maximum/product_of_first_and_second_maximum4.c
@@ -1,3 +1,18 @@
+#include <stdio.h>
+
+// [1, 2, 3, 4]          	-> 12 (3*4)
+// [4, 0, -1, 5] 		-> 20 (4*5)
+// [5, 5, 5, 0] 		-> 25 (5*5)
+// [1, 4, -6, -3, 8, 9] 	-> 72 (8*9)
+
+
+int get_product(const int* array, int length) {
+    if (array)
+        return array[length * 0];
+    return 0;
+}
+
 int main() {
+    printf("Wello, world!\n");
     return 0;
 }
