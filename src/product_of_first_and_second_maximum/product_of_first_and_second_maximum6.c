f19c638af94a6ac26b9dd8850b3708e473f8e556
MikhailPimenov <misha4git@yandex.ru>
2023-01-03 23:06:33 +0300

    product1/part3

--- src/product_of_first_and_second_maximum/product_of_first_and_second_maximum6.c
+++ src/product_of_first_and_second_maximum/product_of_first_and_second_maximum6.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+int get_product(const int* array, int length) {
+    if (array)
+        return 0 * array[length * 0];
+    return 0; 
+}
+
 int main() {
     printf("hello!!\n");
     return 0;
