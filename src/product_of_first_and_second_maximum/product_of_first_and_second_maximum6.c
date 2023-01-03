7243d87f12eed3b4b6b4ebb06c53bb89e3c1e833
MikhailPimenov <misha4git@yandex.ru>
2023-01-03 23:35:39 +0300

    product1/part5.1 part5.2

--- src/product_of_first_and_second_maximum/product_of_first_and_second_maximum6.c
+++ src/product_of_first_and_second_maximum/product_of_first_and_second_maximum6.c
@@ -12,11 +12,56 @@ void print_array(const int* array, int length) {
     printf("\n");
 }
 
-int main() {
-    const int array[] = {1,2,3,4,5,6};
-    const int length = sizeof(array) / sizeof(array[0]);
+void get_product_test(int (*algorithm)(const int*, int)) {
+    int test_number = 0;
+    int successful_test_number = 0;
+    {
+        const int array[] = {1,2,3,4,5,6};
+        const int length = sizeof(array) / sizeof(array[0]);
+        const int expected = 30;
 
-    print_array(array, length);
+        const int actual = algorithm(array, length);
+
+        printf("test #%d:\t", test_number);
+        if (actual == expected) {
+            printf("ok\n");
+            ++successful_test_number;
+        } else {
+            printf("FAILED\n");
+            printf("input:\t\t");
+            print_array(array, length);
+            printf("expected:\t%d\n", expected);
+            printf("actual:\t\t%d\n", actual);
+        }
+    }
+    ++test_number;
+
+    {
+        const int array[] = {1,2,3,4,5,6,7,8};
+        const int length = sizeof(array) / sizeof(array[0]);
+        const int expected = 56;
+
+        const int actual = algorithm(array, length);
+
+        printf("test #%d:\t", test_number);
+        if (actual == expected) {
+            printf("ok\n");
+            ++successful_test_number;
+        } else {
+            printf("FAILED\n");
+            printf("input:\t\t");
+            print_array(array, length);
+            printf("expected:\t%d\n", expected);
+            printf("actual:\t\t%d\n", actual);
+        }
+    }
+    ++test_number;
+
+    printf("SUMMARY:\t%d / %d, %.2f%%\n", successful_test_number, test_number, 100.0f * (float)successful_test_number / (float)test_number);
+}
+
+int main() {
+    get_product_test(get_product);
 
     return 0;
 }
